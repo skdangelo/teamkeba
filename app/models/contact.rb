a7dd5cb62cb68b8acaaf56ff8ff3fdbb5f898ce6
Sandra D'Angelo <skdangelo@gmail.com>
2019-09-29 22:55:30 +0000

    Fixing bugs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
-class Contact < ApplicationRecord
-  after_create :send_contact_email
+class Contact
+  include ActiveModel::Model
+  attr_accessor :name, :email, :subject, :message
+  validates :name, :email, :subject, :message, presence: true
 
   def send_contact_email
       NotificationMailer.contact_added(self).deliver_now
