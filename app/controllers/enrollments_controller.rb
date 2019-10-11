2578e57aef1f5f26cdb7862e1af8c7273362243f
Sandra D'Angelo <skdangelo@gmail.com>
2019-10-11 04:43:12 +0000

    Fixed security and stripe routing

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,49 +3,31 @@ class EnrollmentsController < ApplicationController
 
   def create
     if current_program.premium?
-       # # Amount in cents
-       # @amount = (current_program.cost * 100).to_i
-       #  customer = Stripe::Customer.create(
-       #   email: params[:stripeEmail],
-       #   source: params[:stripeToken]
-       # )
+       # Amount in cents
+       @amount = (current_program.cost * 100).to_i
+       customer = Stripe::Customer.create(
+         email: params[:stripeEmail],
+         source: params[:stripeToken]
+       )
 
-       # charge = Stripe::Charge.create(
-       #   customer: customer.id,
-       #   amount: @amount,
-       #   description: 'Flixter Premo Content',
-       #   currency: 'usd'
-       # )
-      @enrollment = Enrollment.new(enrollment_params)
-      if @registration.save
-        redirect_to @enrollment.paypal_url(enrollment_path(@enrollment))
-      else
-        render :new
-      end    
+       charge = Stripe::Charge.create(
+         customer: customer.id,
+         amount: @amount,
+         description: 'Flixter Premo Content',
+         currency: 'usd'
+       )
     end
 
     current_user.enrollments.create(program: current_program)
-    redirect_to course_path(current_program)
+    redirect_to program_path(current_program)
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to root_path
-  end
+    end
 
   private
 
   def current_program
     @current_program ||= Program.find(params[:program_id])
   end  
-
-
-  protect_from_forgery except: [:hook]
-  def hook
-    params.permit! # Permit all Paypal input params
-    status = params[:payment_status]
-    if status == "Completed"
-      @enrollment = Enrollment.find params[:invoice]
-      @enrollment.update_attributes notification_params: params, status: status, transaction_id: params[:txn_id], purchased_at: Time.now
-    end
-    render nothing: true
-  end
 end
