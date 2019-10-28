400159223fa9034a9b38d5a028a2095927c41b59
Sandra D'Angelo <skdangelo@gmail.com>
2019-10-28 19:17:21 +0000

    Fixed Password reset issue

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Teamkeba
     config.load_defaults 5.2
     config.assets.paths << "#{Rails.root}/app/assets/videos"
     config.assets.paths << "#{Rails.root}/app/assets/pdfs"
-    config.action_mailer.default_url_options = { host: 'info@kpvaelites.org' }
+    config.action_mailer.default_url_options = { host: 'info@teamkeba.com' }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
