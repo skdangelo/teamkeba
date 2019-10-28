9a1e39395304f2d03c57aac8c9942864a36a0ad0
Sandra D'Angelo <skdangelo@gmail.com>
2019-10-28 20:00:12 +0000

    Fixed Password reset issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :test
   #config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.perform_deliveries = true
   #config.action_mailer.smtp_settings = {
   #    address: "smtp.gmail.com",
   #    port: 587,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
