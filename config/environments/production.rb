ed2f52b975b95baf0912b1f46fbdeca60ee85c5a
Sandra D'Angelo <skdangelo@gmail.com>
2019-10-28 20:39:00 +0000

    Fixed Password reset issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,14 @@
 Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'teamkeba.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp.sendgrid.net',
     port: '587',
-    main: 'teamkeba.herokuapp.com',
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
     authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
   
